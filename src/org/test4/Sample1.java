a5ab325cc589e9c40897dce02be8e52dac048cdc
gayathi694 <gaya63672@gmail.com>
2021-04-09 13:10:28 +0530

    MasterTask Done

---
+++ src/org/test4/Sample1.java
@@ -0,0 +1,23 @@
+package org.test4;
+
+public class Sample1 {
+
+		public void sample1() {
+		System.out.println("Sample1");
+		}
+		public void sample2() {
+			System.out.println("Sample2");
+			}
+		public void sample3() {
+			System.out.println("Sample3");
+			}
+
+		public static void main(String[] args) {
+			Sample1 git = new Sample1();
+			git.sample1();
+			git.sample2();
+			git.sample3();
+		}
+
+	}
+
