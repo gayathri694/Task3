5277c5658f8d0c9028680c3f0590bf031f8a6008
amruthasreeg <amruthasreeg1993@gmail.com>
2021-04-09 14:38:52 +0530

    Done

--- src/org/test4/Sample1.java
+++ src/org/test4/Sample1.java
@@ -11,12 +11,25 @@ public class Sample1 {
 		public void sample3() {
 			System.out.println("Sample3");
 			}
+		public void sample7() {
+			System.out.println("Sample1");
+			}
+			public void sample8() {
+				System.out.println("Sample2");
+				}
+			public void sample9() {
+				System.out.println("Sample3");
+				}
 
+}
 		public static void main(String[] args) {
 			Sample1 git = new Sample1();
 			git.sample1();
 			git.sample2();
 			git.sample3();
+			git.sample7();
+			git.sample8();
+			git.sample9();
 		}
 
 	}
